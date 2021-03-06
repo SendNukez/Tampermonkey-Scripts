4253e086a582d3ab41322b634c55fa4cdd282015
Send_Nukez <erikjulius2701@yahoo.de>
2021-03-06 07:53:32 +0100

    Handled some weird Drop Names and added Settings and Pop-Up Notifications

--- Twitch Prime Auto Rust Drops.user.js
+++ Twitch Prime Auto Rust Drops.user.js
@@ -1,27 +1,50 @@
 // ==UserScript==
 // @name         Twitch Prime Auto Rust Drops
 // @namespace    https://twitch.facepunch.com/
-// @version      0.6.7
+// @version      1.0.0
 // @updateURL    https://raw.githubusercontent.com/ErikS270102/Tampermonkey-Scripts/master/Twitch%20Prime%20Auto%20Rust%20Drops.user.js
 // @downloadURL  https://raw.githubusercontent.com/ErikS270102/Tampermonkey-Scripts/master/Twitch%20Prime%20Auto%20Rust%20Drops.user.js
-// @description  Automatically switches to Rust Streamers that have Drops enabled if url has the "drops" parameter set. (Non-Channel-Specific Drops wont get shown as uncompleted, but by the time the others are done they are too)
+// @description  Automatically switches to Rust Streamers that have Drops enabled if url has the "drops" parameter set.
 // @author       Erik
 // @match        https://www.twitch.tv/drops/inventory?checkonly
 // @match        https://twitch.facepunch.com/*
 // @match        https://www.twitch.tv/*?rustdrops
+// @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/js/iziToast.min.js
+// @resource     iziToast https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/css/iziToast.min.css
 // @grant        GM_openInTab
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_addValueChangeListener
+// @grant        GM_registerMenuCommand
 // @grant        GM_notification
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (async () => {
     "use strict";
 
-    var queryInterval;
-    var reloadTimeout;
+    GM_addStyle(GM_getResourceText("iziToast"));
+
+    GM_config.init({
+        id: "Config",
+        fields: {
+            notifications: {
+                label: "Enable Notifications",
+                type: "checkbox",
+                default: true
+            }
+        }
+    });
+    GM_registerMenuCommand("Settings", () => {
+        GM_config.open();
+    });
+
+    window.queryInterval = null;
+    window.reloadTimeout = null;
 
     function sleep(ms) {
         return new Promise((resolve) => setTimeout(resolve, ms));
@@ -44,47 +67,70 @@
         GM_openInTab("https://www.twitch.tv/drops/inventory?checkonly", { active: false, insert: true });
     }
 
+    function sendNotification(title, message, iconUrl = "https://twitch.facepunch.com/favicon.png") {
+        if (GM_config.get("notifications")) GM_notification(title, message, iconUrl);
+        console.log(title, message, iconUrl);
+        iziToast.show({
+            title,
+            message,
+            iconUrl,
+            color: "#9147ff",
+            theme: "dark",
+            position: "topCenter",
+            timeout: 10000,
+            transitionIn: "bounceInDown",
+            transitionOut: "fadeOutUp"
+        });
+    }
+
     $(async () => {
         if (location.host == "twitch.facepunch.com") {
             if (location.search == "?checkonly") {
-                const drops = $("section.streamer-drops a")
+                const drops = $("section.streamer-drops a, section.general-drops a")
                     .toArray()
                     .map((elem) => {
                         return { name: $(elem).find(".drop-footer > .drop-name").text(), url: $(elem).attr("href") + "?rustdrops", live: $(elem).hasClass("is-live") };
                     });
-                setTimeout(() => {
-                    sendMessage("drops", { type: "FACEPUNCH", drops });
-                    window.close();
-                }, 1000);
+
+                sendMessage("drops", { type: "FACEPUNCH", drops });
+                window.close();
             } else {
-                $("section.streamer-drops a").map((i, elem) => {
+                $("section.streamer-drops a, section.general-drops a").map((i, elem) => {
                     const old = elem.getAttribute("href");
-                    elem.setAttribute("href", old + "?rustdrops");
+                    if (new URL(old).host != "www.youtube.com") elem.setAttribute("href", old + "?rustdrops");
                     return elem;
                 });
             }
         } else if (location.href == "https://www.twitch.tv/drops/inventory?checkonly") {
+            let tries = 0;
             do {
-                console.log($(`[data-test-selector="drops-list__wrapper"] > .tw-tower > .tw-flex`).length);
+                if (tries == 100) location.reload();
+                tries++;
                 await sleep(100);
             } while ($(`[data-test-selector="drops-list__wrapper"] > .tw-tower > .tw-flex`).length == 0);
+            await sleep(200);
 
-            $(`[data-test-selector="DropsCampaignInProgressRewardPresentation-claim-button"]`).trigger("click");
-            await sleep(4000);
+            let claimed = false;
+            $(`[data-test-selector="DropsCampaignInProgressRewardPresentation-claim-button"]`).each(function () {
+                const parent = $(this).closest(".tw-flex-grow-1");
+                sendMessage("drops", { type: "CLAIMED", name: parent.find(".tw-pd-1").text(), image: parent.find("img").attr("src") });
+                $(this).trigger("click");
+                claimed = true;
+            });
+
+            if (claimed) await sleep(4000);
+
             const drops = $(`[data-test-selector="drops-list__wrapper"] > .tw-tower > .tw-flex`)
                 .toArray()
                 .filter((e) => {
-                    return $(e).find(`[data-test-selector="awarded-drop__game-name"]`).text() == "Rust";
+                    const agoArr = $(e).find(".tw-c-text-alt-2").first().text().split(" ");
+                    const twoWeeksAgo = agoArr[0] == "yesterday" || (agoArr.length == 3 && Math.abs(moment().subtract(agoArr[0], agoArr[1]).diff(moment(), "day")) <= 8);
+                    return $(e).find(`[data-test-selector="awarded-drop__game-name"]`).text() == "Rust" && twoWeeksAgo;
                 })
-                .map((e) => {
-                    return $(e).find(`[data-test-selector="awarded-drop__drop-name"]`).text();
-                });
-            console.log(drops);
+                .map((e) => $(e).find(`[data-test-selector="awarded-drop__drop-name"]`).text());
 
-            setTimeout(() => {
-                sendMessage("drops", { type: "TWITCH", drops });
-                window.close();
-            }, 1000);
+            sendMessage("drops", { type: "TWITCH", drops });
+            window.close();
         } else if (location.host == "www.twitch.tv") {
             let fpDrops = [];
             let twDrops = [];
@@ -94,36 +140,44 @@
                 console.log("[Auto Rust Drops] MSG:", msg);
                 if (msg.type == "TWITCH") twDrops = msg.drops;
                 if (msg.type == "FACEPUNCH") fpDrops = msg.drops;
+                if (msg.type == "CLAIMED") {
+                    console.log(`[Auto Rust Drops] %c${msg.name} %chas been Claimed!`, "color: #9147ff; font-weight: bold;", "color: none");
+                    sendNotification("Rust Drop Claimed!", `Claimed ${msg.name}!`, msg.image);
+                }
 
                 if (msg.type == "TWITCH" && fpDrops.length > 0) {
                     function rpl(s) {
-                        return s.toLowerCase().replace(/[-_\s]/, "");
+                        return s.toLowerCase().replace(/[\/-_\s0-9]/g, "");
                     }
-                    remainingDrops = fpDrops.filter((fp) => !twDrops.some((tw) => rpl(tw) == rpl(fp.name) || rpl(tw).startsWith(new URL(fp.url).pathname.substring(1))));
+                    function rpl1st(s) {
+                        return rpl(s.split(" ")[0]);
+                    }
+                    remainingDrops = fpDrops.filter((fp) => !twDrops.some((tw) => rpl(tw) == rpl(fp.name) || rpl1st(tw) == rpl1st(fp.name) || rpl(tw).startsWith(rpl(new URL(fp.url).pathname))));
                     remainingDropsLive = remainingDrops.filter((drop) => drop.live);
 
                     const currentDrop = remainingDropsLive.find((drop) => drop.url == location.href);
                     if (currentDrop) {
-                        console.log(`[Auto Rust Drops] %c${currentDrop.name} %cStill not claimed`, "color: purple; font-weight: bold;", "color: none");
+                        console.log(`[Auto Rust Drops] Still waiting for %c${currentDrop.name}%c...`, "color: #9147ff; font-weight: bold;", "color: none");
                     } else if (remainingDrops.length > 0) {
                         if (remainingDropsLive.length > 0) {
                             location.assign(remainingDropsLive[0].url);
                         } else {
-                            console.log(`[Auto Rust Drops] %cNobody Online :( %cRemaining: %c\n${remainingDrops.map((drop) => drop.name).join("\n")}`, "color: red; font-weight: bold;", "color: none", "color: purple; font-weight: bold;");
+                            console.log(`[Auto Rust Drops] %cNobody Online :( %cRemaining: %c\n${remainingDrops.map((drop) => drop.name).join("\n")}`, "color: red; font-weight: bold;", "color: none", "color: #9147ff; font-weight: bold;");
                             const firstLive = fpDrops.find((fp) => fp.live);
                             if (firstLive && location.href != firstLive.url) location.assign(firstLive.url);
                         }
                     } else if (remainingDrops.length == 0) {
-                        clearInterval(queryInterval);
-                        clearTimeout(reloadTimeout);
-                        GM_notification("All Rust Drops Claimed!", "All Rust Twitch Prime Drops have been claimed!", "https://twitch.facepunch.com/favicon.png");
+                        clearInterval(window.queryInterval);
+                        clearTimeout(window.reloadTimeout);
+                        console.log(`[Auto Rust Drops] %cAll Drops %chave been Claimed!`, "color: #9147ff; font-weight: bold;", "color: none");
+                        sendNotification("All Rust Drops Claimed!", "All Rust Twitch Prime Drops have been claimed!");
                     }
                 }
             });
 
             openQueryTabs();
-            queryInterval = setInterval(openQueryTabs, 5 * 60000); // Check for Drops every 5min
-            reloadTimeout = setTimeout(location.reload, 30 * 60000); // Reload every 30min (Just to make sure Stream is Running)
+            window.queryInterval = setInterval(openQueryTabs, 5 * 60000); // Check for Drops every 5min
+            window.reloadTimeout = setTimeout(location.reload, 30 * 60000); // Reload every 30min (Just to make sure Stream is Running)
         }
     });
 })();
