f38579cb03c208f8e0649f1b9b121e8d65ccf060
Send_Nukez <erikjulius2701@yahoo.de>
2021-03-08 06:54:39 +0100

    1.2 Changed Match Detection

--- scripts/Twitch Auto Claim Channel Points.user.js
+++ scripts/Twitch Auto Claim Channel Points.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Twitch Auto Claim Channel Points
 // @homepage     https://www.twitch.tv/
-// @version      1.1
+// @version      1.2
 // @downloadURL  https://github.com/ErikS270102/Tampermonkey-Scripts/raw/master/scripts/Twitch%20Auto%20Claim%20Channel%20Points.user.js
 // @description  Automatically claims Twitch Channel Points
 // @author       Erik
-// @match        https://www.twitch.tv/*
+// @match        https://www.twitch.tv/**
 // @noframes
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @grant        none
@@ -14,18 +14,22 @@
 (async () => {
     "use strict";
 
-    let observer = new MutationObserver(() => {
-        const bonus = $(".claimable-bonus__icon");
-        if (bonus.length != 0) {
-            bonus.trigger("click");
-            console.log("[Auto Claimer] Claimed");
-        }
+    if (!/^\/[a-z0-9]+$/i.test(location.pathname)) return;
+
+    $(async () => {
+        let observer = new MutationObserver(() => {
+            const bonus = $(".claimable-bonus__icon");
+            if (bonus.length != 0) {
+                bonus.trigger("click");
+                console.log("[Auto Claimer] Claimed");
+            }
+        });
+
+        do {
+            await new Promise((resolve) => setTimeout(resolve, 100)); // Sleep
+        } while ($(".chat-input").length == 0);
+
+        observer.observe($(".chat-input").get(0), { childList: true, subtree: true });
+        console.log("[Auto claimer] Observing for Changes...");
     });
-
-    do {
-        await new Promise((resolve) => setTimeout(resolve, 100)); // Sleep
-    } while ($(".chat-input").length == 0);
-
-    observer.observe($(".chat-input").get(0), { childList: true, subtree: true });
-    console.log("[Auto claimer] Observing for Changes...");
 })();
