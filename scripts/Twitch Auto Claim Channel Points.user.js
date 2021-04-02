35652f3d8afb28acd9d4862599d0aec40f442b05
Send_Nukez <erikjulius2701@yahoo.de>
2021-04-02 22:40:24 +0200

    1.2.1 Styled Console Output

--- scripts/Twitch Auto Claim Channel Points.user.js
+++ scripts/Twitch Auto Claim Channel Points.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitch Auto Claim Channel Points
 // @homepage     https://www.twitch.tv/
-// @version      1.2
+// @version      1.2.1
 // @downloadURL  https://github.com/ErikS270102/Tampermonkey-Scripts/raw/master/scripts/Twitch%20Auto%20Claim%20Channel%20Points.user.js
 // @description  Automatically claims Twitch Channel Points
 // @author       Erik
@@ -16,12 +16,16 @@
 
     if (!/^\/[a-z0-9]+$/i.test(location.pathname)) return;
 
+    function log(...data) {
+        console.log("%cChanel Points Claimer:", "color: #18181b; background-color: #31c3a2; padding: 2px 5px; border-radius: 5px; font-weight: bold;", ...data);
+    }
+
     $(async () => {
         let observer = new MutationObserver(() => {
             const bonus = $(".claimable-bonus__icon");
             if (bonus.length != 0) {
                 bonus.trigger("click");
-                console.log("[Auto Claimer] Claimed");
+                log("Claimed");
             }
         });
 
@@ -30,6 +34,6 @@
         } while ($(".chat-input").length == 0);
 
         observer.observe($(".chat-input").get(0), { childList: true, subtree: true });
-        console.log("[Auto claimer] Observing for Changes...");
+        log("Observing for Changes...");
     });
 })();
